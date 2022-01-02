c236e22f7fb3f44ddf0fd778aeb05a44b20a05f5
Carlos Miguel <carlos.miguel.oliveira.17@gmail.com>
2022-01-02 16:54:41 -0300

    feat: :sparkles: Finishing context menu with react-native-hold-menu

--- src/screens/ContextMenu/ContextMenu.tsx
+++ src/screens/ContextMenu/ContextMenu.tsx
@@ -1,11 +1,105 @@
-import { Text } from 'react-native'
+import { HoldMenuProvider, HoldItem } from 'react-native-hold-menu'
+import FeatherIcon from 'react-native-vector-icons/Feather'
 
-import { Container } from './styles'
+import {
+  Container,
+  Content,
+  Item,
+  Row,
+  DotBottomLeft,
+  DotTopCenter,
+  DotTopRight,
+  DotTopLeft,
+  DotBottomCenter,
+  DotBottomRight
+} from './styles'
+
+const items = [
+  {
+    text: 'Actions',
+    // icon: 'home',
+    isTitle: true,
+    onPress: () => console.log('Actions')
+  },
+  {
+    text: 'Action 1',
+    // icon: 'edit',
+    onPress: () => console.log('Action 1')
+  },
+  {
+    text: 'Action 2',
+    // icon: 'map-pin',
+    withSeparator: true,
+    onPress: () => console.log('Action 2')
+  },
+  {
+    text: 'Action 3',
+    // icon: 'trash',
+    isDestructive: true,
+    onPress: () => console.log('Action 3')
+  }
+]
 
 export function ContextMenu() {
   return (
-    <Container>
-      <Text>ContextMenu</Text>
-    </Container>
+    <HoldMenuProvider iconComponent={FeatherIcon} theme='dark'>
+      <Container>
+        <Content>
+          <Row>
+            <HoldItem items={items} menuAnchorPosition='bottom-left'>
+              <Item>
+                <DotBottomLeft />
+              </Item>
+            </HoldItem>
+            <HoldItem items={items}>
+              <Item>
+                <DotTopCenter />
+              </Item>
+            </HoldItem>
+            <HoldItem items={items}>
+              <Item>
+                <DotTopRight />
+              </Item>
+            </HoldItem>
+          </Row>
+
+          <Row>
+            <HoldItem items={items}>
+              <Item>
+                <DotTopLeft />
+              </Item>
+            </HoldItem>
+            <HoldItem items={items}>
+              <Item>
+                <DotTopCenter />
+              </Item>
+            </HoldItem>
+            <HoldItem items={items}>
+              <Item>
+                <DotTopRight />
+              </Item>
+            </HoldItem>
+          </Row>
+
+          <Row>
+            <HoldItem items={items}>
+              <Item>
+                <DotBottomLeft />
+              </Item>
+            </HoldItem>
+            <HoldItem items={items}>
+              <Item>
+                <DotBottomCenter />
+              </Item>
+            </HoldItem>
+            <HoldItem items={items}>
+              <Item>
+                <DotBottomRight />
+              </Item>
+            </HoldItem>
+          </Row>
+        </Content>
+      </Container>
+    </HoldMenuProvider>
   )
 }
--- src/screens/ContextMenu/styles.ts
+++ src/screens/ContextMenu/styles.ts
@@ -1,3 +1,84 @@
 import styled from 'styled-components/native'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
-export const Container = styled.View``
+import { WINDOW_WIDTH } from '../../constants'
+
+export const Container = styled(SafeAreaView)`
+  flex: 1;
+  width: ${WINDOW_WIDTH}px;
+
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const Content = styled.View`
+  flex: 1;
+  width: 100%;
+
+  flex-direction: column;
+  justify-content: space-between;
+`
+
+export const Item = styled.View`
+  width: ${WINDOW_WIDTH / 5}px;
+  height: 70px;
+  padding-top: ${props => props.theme.spacing}px;
+
+  background: ${props => props.theme.colors.primary.normal};
+  border-radius: ${props => props.theme.spacing * 1.5}px;
+
+  justify-content: center;
+  align-items: center;
+`
+
+export const Row = styled.View`
+  width: 100%;
+
+  flex-direction: row;
+  justify-content: space-between;
+  margin-bottom: ${props => props.theme.spacing * 2}px;
+  padding: ${props => props.theme.spacing * 4}px;
+`
+
+const Dot = styled.View`
+  width: 20px;
+  height: 20px;
+
+  background: ${props => props.theme.colors.primary.lighter};
+  border-radius: 10px;
+`
+
+export const DotTopRight = styled(Dot)`
+  position: absolute;
+  top: -5px;
+  right: -5px;
+`
+
+export const DotTopLeft = styled(Dot)`
+  position: absolute;
+  top: -5px;
+  left: -5px;
+`
+
+export const DotTopCenter = styled(Dot)`
+  position: absolute;
+  top: -5px;
+`
+
+export const DotBottomRight = styled(Dot)`
+  position: absolute;
+  bottom: -5px;
+  right: -5px;
+`
+
+export const DotBottomLeft = styled(Dot)`
+  position: absolute;
+  bottom: -5px;
+  left: -5px;
+`
+
+export const DotBottomCenter = styled(Dot)`
+  position: absolute;
+  bottom: -5px;
+`
